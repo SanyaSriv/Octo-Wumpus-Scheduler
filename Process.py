0e9c92a47a0261c64db394bcdaeb77de91b4e6b5
SanyaSriv <ssrivas7@ucsc.edu>
2024-03-19 02:46:21 -0700

    Adding the base code for Process.py, needs balancing code

--- Process.py
+++ Process.py
@@ -29,3 +29,59 @@ class Node:
     def reset_num_lottery_chances(self):
         """Should reset after every epoch."""
         self.num_lottery_chances = 0
+
+class ProcessTree:
+    """This is the process tree that we will iterate over for selecting the winners."""
+    def __init__(self):
+        self.root = None
+    
+    def add_node(self, node):
+        """Will add a node to the tree according to the left and right range."""
+        if self.root is None:
+            # this new node is going to be the root node
+            self.root = node
+            self.root.left_node = None
+            self.root.right_node = None
+        else:
+            self.add_node_helper(self.root, node) # add it as a child of another node
+    
+    def add_node_helper(self, parent_node, node):
+        if node.left_range > parent_node.right_range:
+            # node should be the right child
+            if parent_node.right_node is None:
+                parent_node.right_node = node
+                parent_node.right_node.height = parent_node.height + 1
+            else:
+                self.add_node_helper(parent_node.right_node, node)
+        elif node.right_range < parent_node.left_range:
+            # node should be the left child
+            if parent_node.left_child is None:
+                parent_node.left_child = node
+                parent_node.left_child.height = parent_node.height + 1
+            else:
+                self.add_node_helper(parent_node.left_child, node)
+    
+    def find_lottery_winner_helper(self, node, t):
+        if node.left_range <= t <= node.right_range:
+            # winner found!
+            return node
+        if t < node.left_range:
+            # recurse over the left subtree
+            if node.left_child is not None:
+                self.find_lottery_winner_helper(node.left_child, t)
+            else:
+                return None
+        else:
+            # recurse over the right subtree
+            if node.right_child is not None:
+                self.find_lottery_winner_helper(node.right_child, t)
+            else:
+                return None
+
+    def find_lottery_winner(self, winning_ticket):
+        """Will return the winning node"""
+        w = self.find_lottery_winner_helper(self.root, winning_ticket)
+        if w is None:
+            print("An invalid winner was selected -> winner is not in the process tree.")
+            return None
+        return w
